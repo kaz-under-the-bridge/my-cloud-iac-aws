7de5715eb74b7e7d50f0f4a9cb48c7ca9160baa2
lg-kaz-utb <kazuhiro.hashimoto@loglass.co.jp>
2025-05-06 15:12:58 +0900

    VPC定義用のcloudformationを追加

---
+++ terraform/envs/kaz-under-the-bridge/CFn.tf
@@ -0,0 +1,17 @@
+locals {
+  cfn_templates = [
+    {
+      name     = "vpc-network-configuration"
+      template = file("${path.module}/vpc_cloudformation.tpl")
+    }
+  ]
+}
+
+resource "aws_cloudformation_stack" "main" {
+  for_each = { for tpl in local.cfn_templates : tpl.name => tpl }
+
+  name          = each.value.name
+  template_body = each.value.template
+
+  capabilities = ["CAPABILITY_NAMED_IAM"]
+}
