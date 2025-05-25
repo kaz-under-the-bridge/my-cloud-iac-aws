c661efcbdc2fe6e27db19520479e316eb668a35d
lg-kaz-utb <kazuhiro.hashimoto@loglass.co.jp>
2025-05-25 11:47:21 +0900

    VPCリソース定義を更新（テスト用のvpc削除・CFn化からTF resourceへ）

--- terraform/envs/kaz-under-the-bridge/CFn.tf
+++ terraform/envs/kaz-under-the-bridge/CFn.tf
@@ -1,17 +1,19 @@
-locals {
-  cfn_templates = [
-    {
-      name     = "vpc-network-configuration"
-      template = file("${path.module}/vpc_cloudformation.tpl")
-    }
-  ]
-}
-
-resource "aws_cloudformation_stack" "main" {
-  for_each = { for tpl in local.cfn_templates : tpl.name => tpl }
-
-  name          = each.value.name
-  template_body = each.value.template
-
-  capabilities = ["CAPABILITY_NAMED_IAM"]
-}
+# cloudformationでVPC定義を試したときのサンプルコード(テンプレートは削除済)
+#locals {
+#  cfn_templates = [
+#    {
+#      name     = "vpc-network-configuration"
+#      template = file("${path.module}/vpc_cloudformation.tpl")
+#    }
+#  ]
+#}
+#
+#resource "aws_cloudformation_stack" "main" {
+#  for_each = { for tpl in local.cfn_templates : tpl.name => tpl }
+#
+#  name          = each.value.name
+#  template_body = each.value.template
+#
+#  capabilities = ["CAPABILITY_NAMED_IAM"]
+#}
+#
